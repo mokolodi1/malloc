d20ab7697cf2674ef66a813ef9ff8932f010d623
Teo Fleming <mokolodi1@gmail.com>
2017-05-18 19:32:29 +0200

    did work

--- libft/libft/rb_trees/ft_rb_put.c
+++ libft/libft/rb_trees/ft_rb_put.c
@@ -12,8 +12,54 @@
 
 #include "rb_trees.h"
 
-t_rb_node			*put(t_rb_node *subtree_root, void *key, void *value)
+t_rb_node			*create_node(void *key, void *value)
 {
+	t_rb_node		*new_node;
+
+	new_node = malloc(sizeof(t_rb_node));
+	new_node->key = key;
+	new_node->value = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->color = RB_RED;
+	new_node->size = 1;
+	return (new_node);
+}
+
+t_rb_node			*rotate_left(t_rb_node *node)
+{
+	
+}
+
+t_rb_node			*rotate_right(t_rb_node *node)
+{
+	
+}
+
+// TODO: flip_colors
+
+t_rb_node			*put(t_rb_node *node, int (*compare)(void*, void*)
+							, void *key, void *value)
+{
+	int				comparison;
+
+	if (!node)
+		return (create_node(key, value));
+	comparison = compare(key, node->key);
+	if (comparison < 0)
+		node->left = put(node->left, key, value);
+	else if (comparison > 0)
+		node->right = put(node->right, key, value);
+	else
+		node->value = value;
+	if (is_red(node->right) && !is_red(node->left))
+		node = rotate_left(node);
+	if (is_red(node->left) && is_red(node->left->left))
+		node = rotate_right(node);
+	if (is_red(node->left) && is_red(node->right))
+		flip_colors(node);
+	node->size = size(node->left) + size(node->right) + 1;
+	return (node);
 	
 }
 
